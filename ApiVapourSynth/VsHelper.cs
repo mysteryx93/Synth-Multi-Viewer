30be753405bb3fd5a4bbb018e9bd1afb3b08cb8b
Etienne Charland <mysteryx93@protonmail.com>
2026-09-16 05:58:59 -0500

    Re-add unsafe

--- ApiVapourSynth/VsHelper.cs
+++ ApiVapourSynth/VsHelper.cs
@@ -46,7 +46,7 @@ public static class VsHelper
     /// <summary>
     /// Copies frame data from one memory location to another.
     /// </summary>
-    public static void BitBlt(IntPtr dstp, int dstStride, IntPtr srcp, int srcStride, int rowSize, int height)
+    public static unsafe void BitBlt(IntPtr dstp, int dstStride, IntPtr srcp, int srcStride, int rowSize, int height)
     {
         for (var row = 0; row < height; row++)
         {
